424837b15e38df16fe144eb78578f7e9f5a17166
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-15 15:21:41 -0300

    Primeiro programa em PHP

---
+++ helloword.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Primeiro Programa</title>
+	<style>
+		h2{text-align: center; 
+		color: #4E4E4E;
+		}
+
+	</style>
+</head>
+<body>
+	<h1>Testando php</h1>
+	<?php
+	echo "<h2>Hello Word!<h2>";
+	?>
+</body>
+</html>
