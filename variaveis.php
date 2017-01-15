81d038a613614b067dd8227e3f964b720254fc13
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-15 15:22:10 -0300

    Aula Curso em video sobre Variaveis

---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Variaveis PHP</title>
+</head>
+<body>
+<h1>Variaveis com PHP</h1>
+</br>
+	<?php 
+		$idade = 18;
+		$sal = 1825.54;
+		$nome = "Leonardo";
+		$casado = false;
+		echo "$nome tem $idade anos" ;
+		echo "</br>";
+	?>
+</body>
+</html>
