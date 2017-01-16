8e8929dc0dc6a38f61e3486ad7aa4ed77ad79c5c
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-16 14:52:02 -0300

    add atribuições

---
+++ atribuicoes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>atribuições.php</title>
+</head>
+<body>
+<?php 
+	$a = 3;
+	$b = &$a;
+	echo "$b";
+
+ ?>
+	
+</body>
+</html>
