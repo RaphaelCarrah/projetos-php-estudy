5bac989e62b5428fbb2aa088b0623d369394b52f
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-19 15:37:33 -0300

    Operadores2

---
+++ operadoresternarios2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Operadores TErnarios 2 </title>
+</head>
+<body>
+	<?php 
+		$nota1 = 7; 
+		$nota2 = 7;
+		$media = ($nota1 + $nota2)/2;
+		$result = ($media > 6)? "Aprovado" : "Reprovado";
+		echo "Sua nota1 é "
+		echo $result;
+	 ?>
+</body>
+</html>
